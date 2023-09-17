803073436beb2c565994f7765ffdaf3d27d034bc
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-09-17 06:43:11 +0530

    second commit

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
-import {boolean} from "zod";
 
 function Form() {
     const [inputValue, setInputValue] = useState<string>('');
@@ -16,15 +15,10 @@ function Form() {
         }
 
         if (authenticated) {
-            // Handle form submission here, e.g., send inputValue to the server
-
             console.log(inputValue);
-
-            // Clear the form input field
             setInputValue('');
 
-            // Navigate to the success page only if authenticated
-            router.push('/success'); // Replace '/success' with the actual URL of the page you want to navigate to
+            router.push('https://muj.acm.org/#/x');
         } else {
             // Handle authentication error or show a message
             console.log('Authentication failed');
@@ -32,20 +26,23 @@ function Form() {
     };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <form onSubmit={handleSubmit} className={"justify-items-center"}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <form onSubmit={handleSubmit} className="bg-transparent m-16 justify-items-center flex flex-col items-center justify-center">
+                <h1 className="mt-10 text-4xl font-bold font-display">Decode</h1>
+                <h2 className="mt-10 mb-3">Enter Discord ID to start...</h2>
                 <input
                     type="text"
-                    placeholder="Enter Discord ID(ex-Koala#6578)"
+                    placeholder="example - Koala#6578"
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                     style={{ padding: '10px', marginRight: '10px' }}
-                    className={"bg-opacity-50 rounded-lg text-black border border-gray-400 p-2 w-full h-12"}
+                    className="bg-opacity-50 rounded-lg text-black border border-gray-400 p-2 w-full h-12"
                 />
-                <button type="submit" style={{ padding: '10px' }} className={"mt-10 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"}>
+                <button type="submit" style={{ padding: '10px' }} className="mt-2 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                     Submit
                 </button>
             </form>
+
         </div>
     );
 }
